0f576384d3285bda0869a85ba4509a5729c322d2
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-03-15 21:41:16 -0500

    Home and navbar component imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { JsonpModule } from '@angular/http';
+import { BrowserModule }    from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { JsonpModule }      from '@angular/http';
 
 
-import { AppComponent } from './app.component';
+import { AppComponent }     from './app.component';
+//My components
+import { NavbarComponent }  from './components/shared/navbar/navbar.component';
+import { HomeComponent }    from './components/home/home.component';
 
 //Services
-import { MoviesService } from './services/movies.component';
+import { MoviesService }    from './services/movies.component';
+
+//Routes
+import { APP_ROUTING }      from './app.routes';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    JsonpModule
+    JsonpModule,
+    APP_ROUTING
   ],
   providers: [
     MoviesService
