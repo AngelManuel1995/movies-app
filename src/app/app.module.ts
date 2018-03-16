696a343a8827ded40fc25d5aa01af2292039d59c
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-03-15 20:43:01 -0500

    JSONP imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { JsonpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+
+//Services
+import { MoviesService } from './services/movies.component';
 
 
 @NgModule({
@@ -10,9 +14,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    JsonpModule
   ],
-  providers: [],
+  providers: [
+    MoviesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
