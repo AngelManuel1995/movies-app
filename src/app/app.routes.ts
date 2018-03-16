c48d3033f8454201eda6b93eac6e07963d864764
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-03-16 15:41:09 -0500

    Routes file created

---
+++ src/app/app.routes.ts
@@ -0,0 +1,30 @@
+//Routes nos permite crear el arreglo de rutas.
+//RouterModule nos permite exportar el arreglo
+//Las importamos desde @angular/router
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent }    from './components/home/home.component';
+import { SearchComponent }  from './components/search/search.component';
+import { MovieComponent }   from './components/movie/movie.component';
+
+
+const APP_ROUTES:Routes = [
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {
+        path:'search', 
+        component:SearchComponent
+    },
+    {
+        path:'movie', 
+        component:MovieComponent
+    },
+	{ 
+        path: '**', 
+        pathMatch:'full', 
+        redirectTo:'home'
+    }
+]
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES)
