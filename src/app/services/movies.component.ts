65c3ec56de9e129d366aa1cb5058c6891c304d45
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-03-17 19:47:14 -0500

    moviesInTheatre method done

--- src/app/services/movies.component.ts
+++ src/app/services/movies.component.ts
@@ -20,4 +20,28 @@ export class MoviesService{
         })
     }
 
+    searchMovie( text:string ){
+        let url = `${this.urlMoviedb}/discover/movie?query=${text}&sort_by=popularity.desc&api_key=${this.apikey}&language=es&callback=JSONP_CALLBACK`;
+        return this._jsonp.get(url).map( res => {
+            console.log(res.json());
+            return res.json();
+        })
+    }
+
+    getMoviesInTheatre(){
+        let desde = new Date();
+        let hasta = new Date();
+        hasta.setDate( desde.getDate() + 7);
+
+        let desdeStr = `${desde.getFullYear()}-${desde.getMonth()+1}-${desde.getDate()}`;
+        let hastaStr = `${hasta.getFullYear()}-${hasta.getMonth()+1}-${hasta.getDate()}`; 
+
+        let url = `${this.urlMoviedb}/discover/movie?primary_release_date.gte=${desdeStr}&primary_release_date.lte=${hastaStr}&api_key=${this.apikey}&language=es&callback=JSONP_CALLBACK`;
+        
+        return this._jsonp.get(url).map(res=>{
+            console.log(res.json());
+            return res.json();
+        })
+    }
+
 }
