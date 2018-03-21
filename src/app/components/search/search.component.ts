d59642a16b38c66cb5d8682de1af14815c2533d8
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-03-21 17:44:32 -0500

    Component created and working

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } 		 from '../../services/movies.component';
 
 @Component({
 	selector:'app-search',
 	templateUrl:'./search.component.html'
 })
 
-export class SearchComponent{
-	constructor(){
-		
+export class SearchComponent implements OnInit{
+	
+	private buscar:string;
+	constructor(private _moviesService:MoviesService){
+	
+	}
+
+	ngOnInit(){
+
+	}
+
+	searchMovie(){
+		if(this.buscar.length === 0){
+			return console.log("Debes escribir algo para buscar");
+		}
+		this._moviesService.searchMovie(this.buscar)
+			.subscribe()
 	}
 }
